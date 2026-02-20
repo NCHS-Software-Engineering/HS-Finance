39b8cab5ffac49bf8a849773425c4792a842c895
Owen Schade <ocschade@stu.naperville203.org>
2026-02-20 13:21:56 -0600

    google auth start

--- hs-finance/app/api/chartAccounts/route.ts
+++ hs-finance/app/api/chartAccounts/route.ts
@@ -2,6 +2,9 @@
 // route.ts is used for an API endpoint with a URL of /api/competencies
 
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { ResultSetHeader, RowDataPacket} from "mysql2/promise";
+import { authOptions } from "@/api/auth/[...nextauth]/authOptions";
 import connection from "@/lib/db";
 
 // A GET request is an HTTP method to retrieve some data from a server based on a request
@@ -10,6 +13,12 @@ export async function GET() {
     // We try to execute a query on our database to select all of the competencies sorted by the skill name
     // The resulting rows are sent as JSON
     try {
+        // Get the session information and check that the session is valid with a user email
+        const session = await getServerSession(authOptions);
+        if (!session || !session.user?.email) {
+            return NextResponse.json({error: "Not Authenticated"}, {status: 401})
+        }
+
         const [rows] = await connection.execute("SELECT ID, AccountName FROM Account ORDER BY AccountName ASC");
         return NextResponse.json(rows);
     }
@@ -18,7 +27,22 @@ export async function GET() {
         console.log(err);
         return NextResponse.json({error: "Failed to fetch chart of accounts."}, {status: 500});
     }
-} 
+};
+/*
 export async function POST(req: Request) {
+    try {
+        const {text} = await req.json();
+
+        const [entryResult] = await connection.execute<ResultSetHeader>(
+            "INSERT INTO Account (text) VALUES (?)",
+            [text]
+        );
+    }
+}
+export async function PATCH(req: Request) {
     
 }
+export async function DELETE(req: Request) {
+    
+}
+*/
