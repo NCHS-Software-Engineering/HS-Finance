96f36a5c0953b125cc26f9a06b5e53fbcdab7078
Owen-Schade <152450132+Owen-Schade@users.noreply.github.com>
2026-03-19 13:31:23 -0500

    started request work

--- hs-finance/app/api/entries/route.ts
+++ hs-finance/app/api/entries/route.ts
@@ -9,7 +9,90 @@ import connection from "@/lib/db";
 
 // A GET request is an HTTP method to retrieve some data from a server based on a request
 // App Router automatically looks for exported functions based on HTTP methods such as GET
-export async function GET() {
+export async function GET(request: Request) {
+    // We try to execute a query on our database to select all of the competencies sorted by the skill name
+    // The resulting rows are sent as JSON
+    try {
+        // Get the session information and check that the session is valid with a user email
+        const session = await getServerSession(authOptions);
+        if (!session || !session.user?.email) {
+            return NextResponse.json({error: "Not Authenticated"}, {status: 401});
+        }
+
+        const { searchParams } = new URL(request.url);
+
+        const type = searchParams.get("type");
+        const rec = searchParams.get("rec");
+        const voided = searchParams.get("void");
+        const startDate = searchParams.get("startDate");
+        const endDate = searchParams.get("endDate");
+        const direction = searchParams.get("direction");
+        const register = searchParams.get("register");
+        const location = searchParams.get("location");
+        const order = searchParams.get("order");
+
+        let query = 
+        `SELECT 
+            Entry.ID, Entry.TransactionID, Entry.Location, Entry.Memo, Entry.Date, Entry.RegisterID, Entry.Void, Entry.Rec, Entry.EntryType
+            FROM Entry, User, Register
+            WHERE User.Email = ? 
+            AND (User.SchoolID = Register.SchoolID OR User.AccountType = 'Dev')
+            AND Register.ID = Entry.RegisterID`;
+        const userEmail = session.user.email;
+        let params = [userEmail];
+
+        if (type) {
+            query += ` AND Entry.EntryType = ?`;
+            params.push(type);
+        }
+        if (rec) {
+            query += ` AND Entry.Rec = ?`;
+            params.push(rec);
+        }
+        if (voided) {
+            query += ` AND Entry.Void = ?`;
+            params.push(voided);
+        }
+
+        if (startDate && endDate) {
+            query += ` AND Entry.Date BETWEEN ? AND ?`;
+            params.push(startDate, endDate);
+        } else if (startDate) {
+            query += ` AND Entry.Date >= ?`;
+            params.push(startDate);
+        } else if (endDate) {
+            query += ` AND Entry.Date <= ?`;
+            params.push(endDate);
+        }
+        
+        if (type) {
+            query += ` AND Entry.EntryType = ?`;
+            params.push(type);
+        }
+        if (type) {
+            query += ` AND Entry.EntryType = ?`;
+            params.push(type);
+        }
+        if (type) {
+            query += ` AND Entry.EntryType = ?`;
+            params.push(type);
+        }
+
+        // Returns all if user account type is Dev, otherwise returns registers for school
+        const [rows] = await connection.execute(
+            query
+        , params);
+        return NextResponse.json(rows);
+    }
+    // If there is an error in the try block, the JSON response sense includes the error message and status code (500 indicates a general server error)
+    catch (err) {
+        console.log(err);
+        return NextResponse.json({error: "Failed to fetch chart of accounts."}, {status: 500});
+    }
+};
+
+
+export async function POST() {
     // We try to execute a query on our database to select all of the competencies sorted by the skill name
     // The resulting rows are sent as JSON
     try {
